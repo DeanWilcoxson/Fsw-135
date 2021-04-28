7847956c52bfaeee8656f86a8ebc8d2adc054e85
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-04-28 00:01:29 -0700

    4/27/21

---
+++ week3/rock_the-vote_pt1/server/routes/router.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router()
+const user = require('../models/user')
+const issue = require('../models/issue')
+const comment = require('../models/comment')
