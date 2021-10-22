324fb3ad7dd9bef21299ce0efa8844de2033d9c6
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-10-21 23:00:39 -0700

    10/21/21

--- week1/server/server.js
+++ week1/server/server.js
@@ -9,16 +9,22 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use("/", router);
 
-mongoose.connect(
-  "mongodb://localhost:27017/inventorydb",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  },
-  () => console.log("Connected to the DB")
-);
+main().catch(err => console.log(err))
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/inventorydb');
+    console.log("Connected to the DB")
+}
+// mongoose.connect(
+//   "mongodb://localhost:27017/inventorydb",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//   },
+//   () => console.log("Connected to the DB")
+// );
 
 app.listen(9000, () => {
   console.log("Server is running on LocalHost:9000");
